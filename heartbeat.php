badef24a1aea09ce4d423acf925e2e718a96f354
Matt Diehl <mdiehl@lexprompt.com>
2018-06-05 09:47:27 -0600

    Switch to use sqlsrv instead of mysqli

--- heartbeat.php
+++ heartbeat.php
@@ -8,37 +8,48 @@ $tag     = $_GET['tag'];
 $event   = $_GET['event'];
 $ext_ip  = $_SERVER['REMOTE_ADDR'];
 
-$mysqli = new mysqli('localhost', 'REPLACE_USERNAME', 'REPLACE_PASSWORD', 'brightsign');
-
-/* Check Connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
+// If not local, //serverName\instanceName and port (default 1433) needed,
+// change "(local)" to "serverName\\sqlexpress, 1542"
+$conn = sqlsrv_connect("(local)", 
+          array(
+		    "UID" => "REPLACE_USERNAME",
+		    "PWD" => "REPLACE_PASSWORD",
+		    "Database" => 'brightsign'));
+if (!$conn){
+    printf("Connect failed: %s\n", print_r( sqlsrv_errors(), true));
 	exit();
 }
-	
-$sql1 = "DELETE FROM heartbeats WHERE snum='$serial';";
-if (!$mysqli->query($sql1)){
-	printf("Error deleting from heartbeats: %s\n", $mysqli->error);
+
+$tsql = "DELETE FROM heartbeats WHERE snum = (?)";
+$params = array($serial);
+
+/* Prepare and execute the query. */
+if (!sqlsrv_query($conn, $tsql, $params)){
+	printf("Error deleting from dbo.heartbeats: %s\n", print_r( sqlsrv_errors(), true));
 	/* close connection */
-	$mysqli->close();
+	sqlsrv_close($conn);
 	exit();
 }
 
 if ($tag=="") $tag="none";
 if ($event=="") $event="none";
 
+$tsql = "INSERT INTO dbo.heartbeats (snum,version,fw,int_ip,ext_ip,tag,event) VALUES (?, ?, ?, ?, ?, ?, ?)";
+$params = array($serial,$version,$fw,$int_ip,$ext_ip,$tag,$event);
 
-$sql2 = "INSERT INTO heartbeats (snum,version,fw,int_ip,ext_ip,tag,event) VALUES ('$serial','$version','$fw','$int_ip','$ext_ip','$tag','$event');";
-if (!$mysqli->query($sql2)){
-	printf("Error inserting into heartbeats: %s\n", $mysqli->error);
-	/* close connection */
-	$mysqli->close();
-	exit();
+/* Prepare and execute the query. */
+$stmt = sqlsrv_query($conn, $tsql, $params);
+if ($stmt){
+	printf("Row inserted successfully.\n");
+	header("Status: 200 OK");
+	/* Free resource */
+	sqlsrv_free_stmt($stmt);
+} else {
+	printf("Error deleting from heartbeats: %s\n", print_r( sqlsrv_errors(), true));
 }
 
 /* close connection */
-$mysqli->close();
+sqlsrv_close($conn);
+exit();
 
-exit(header("Status: 200 OK"));
-	
 ?>
